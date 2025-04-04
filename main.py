05021a7fbf658380e37f79b2a112718429b31d42
eneller <erikneller@gmx.de>
2025-04-04 09:42:47 +0200

    feat: populate template

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from jinja2 import Environment, FileSystemLoader, meta
 
 env = Environment(loader=FileSystemLoader('.'))
-template_source = env.loader.get_source(env, 'index.html')
+filename =  'index.html'
+# get undeclared variables
+template_source = env.loader.get_source(env, filename)
 parsed_content = env.parse(template_source)
 vars = meta.find_undeclared_variables(parsed_content)
-vars = dict.fromkeys(vars, None)
-print(vars)
+vars = dict.fromkeys(sorted(vars), None)
+for var in vars:
+    vars[var] = input(var+ ' :')
+# render template
+template = env.get_template(filename)
+print(template.render(vars))
