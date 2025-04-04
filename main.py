9d16098f96da15f4a0aaa9b447e389424b35b396
eneller <erikneller@gmx.de>
2025-04-04 12:27:23 +0200

    feat: click cli

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from jinja2 import Environment, FileSystemLoader, meta
+import click
 
-env = Environment(loader=FileSystemLoader('.'))
-filename =  'index.html'
-# get undeclared variables
-template_source = env.loader.get_source(env, filename)
-parsed_content = env.parse(template_source)
-vars = meta.find_undeclared_variables(parsed_content)
-vars = dict.fromkeys(sorted(vars), None)
-for var in vars:
-    vars[var] = input(var+ ' :')
-# render template
-template = env.get_template(filename)
-print(template.render(vars))
+@click.command()
+@click.option('--output', '-o', default='output.html', help='Name of Output File')
+def main(output):
+    env = Environment(loader=FileSystemLoader('.'))
+    filename =  'index.html'
+    # get undeclared variables
+    template_source = env.loader.get_source(env, filename)
+    parsed_content = env.parse(template_source)
+    vars = meta.find_undeclared_variables(parsed_content)
+    vars = dict.fromkeys(sorted(vars), None)
+    for var in vars:
+        vars[var] = input(var+ ' :')
+    # render template
+    template = env.get_template(filename)
+    with open(output,'w') as out:
+        out.write(template.render(vars))
+
+if __name__ == '__main__':
+    main()
